50d8c2a6d954cadec9d1d0e77365ea65728f74fd
Anish Pawar <56557214+AnishPawar@users.noreply.github.com>
2020-11-27 21:08:35 +0530

    [Drive_Path_v2_PID]

---
+++ Drive_Path/Drive_Path_v2/Scripts/Drive_Path_v2_Variables.m
@@ -0,0 +1,24 @@
+%% Plane Dimensions
+    plane_length=13.3
+    plane_width=10
+    plane_depth = 0.001/2
+    friction = 0.62
+%% Omni-wheel 
+    %%friction
+    %roller_sfriction= 0.7
+    %roller_kfriction= 0.1
+    wheel_sfriction= 0.7
+    wheel_kfriction = 0.5
+    
+    roller_mass = 0.5
+%% roller contact
+    rad1 = 9.24
+    rad2 = 9.22
+    rad3 = 9.02
+    rad4 = 8.72
+    
+    roller_stiffness= 1e4
+    roller_damping = 1e3
+    
+    roller_static_friction = 0.7
+    roller_dynamic_friction = 0.6
